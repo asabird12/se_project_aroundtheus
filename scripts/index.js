dd6cdcc68258f524bcf99f3bcba1be856116b727
asabird12 <160693729+asabird12@users.noreply.github.com>
2024-05-10 22:22:27 -0400

    sprint 4 stage 1

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const initialCards = {
+    name: ["Yosemite Valley", "Lake Louise", "Bald Mountains", "Latemar", "Vanoise National Park", "Lago di Braies"]
+    link: ["https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg", "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg", "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg", "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg", "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg", "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"]
+}
+
+console.log(initialCards.name);
+console.log(initialCards.link);
