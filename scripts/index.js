e056377e7e9a323e0cf72156fe1b63b1f43c89de
asabird12 <160693729+asabird12@users.noreply.github.com>
2024-05-11 19:02:11 -0400

    sprint 4 stage 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,28 @@
 const initialCards = [
-    parkname: ["Yosemite Valley", "Lake Louise", "Bald Mountains", "Latemar", "Vanoise National Park", "Lago di Braies"]
-    link: ["https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg", "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg", "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg", "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg", "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg", "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"]
-]
+    { 
+        name: "Yosemite Valley"
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
+    },
+    {
+        name: "Lake Louise"
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
+    },
+    {
+        name: "Bald Mountains"
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
+    },
+    {
+        name: "Latemar"
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
+    },
+    {
+        name: "Vanoise National Park"
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
+    },
+    {
+        name: "Lago di Braies"
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
+    }
+];
 
-console.log(initialCards.parkname);
-console.log(initialCards.link);
+console.log(initialCards)
