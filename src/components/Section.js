16f653368789292e8f8a68d9ec59323c11baea37
asabird12 <160693729+asabird12@users.noreply.github.com>
2024-09-20 21:15:02 -0400

    Sprint 8

---
+++ src/components/Section.js
@@ -0,0 +1,6 @@
+export default class Section {
+  constructor({item, renderer}, ) {
+    this._item = ;
+    this._renderer = 
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/pages/index.js",
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    compress: true,
+    port: 5500,
+    open: true,
+    liveReload: true,
+    hot: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/i,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|woff(2)?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
